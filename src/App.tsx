75cdbb940fb4f5b327d57666f0d0f3c215fec718
Kashifnobin-dev <kashifakhternobin02@gmail.com>
2026-09-12 01:48:48 +0600

    Adding App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import TechnologySection from "./components/TechnologySection";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <Hero />
+
+        <TechnologySection />
+      </main>
+
+      <Footer />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2500}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+export default App;
